8e4a54972c029cd79c435d3bd9840b970cd3d714
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-06-27 23:18:48 +0900

    [level 0] Title: 할 일 목록, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public List<String> solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<todo_list.length; i++){
+            if(finished[i] == false){
+                list.add(todo_list[i]);
+            }
+        }
+        return list;
+    }
+}
