dd5257249df91b5f2a9a335f556ccef464dd6ced
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-06-28 16:37:47 +0900

    [level 0] Title: 특이한 정렬, Time: 0.45 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,27 +1,37 @@
 import java.util.*;
+
+class Pair implements Comparable<Pair> {
+    int value;
+    int diff;
+
+    public Pair(int value, int diff) {
+        this.value = value;
+        this.diff = diff;
+    }
+
+    // 거리가 짧은 순으로, 거리가 같으면 숫자가 큰 순으로 정렬
+    @Override
+    public int compareTo(Pair other) {
+        if (this.diff != other.diff) {
+            return this.diff - other.diff;
+        }
+        return other.value - this.value; // 거리가 같을 때는 값이 큰 숫자를 앞으로
+    }
+}
+
 class Solution {
     public List<Integer> solution(int[] numlist, int n) {
-        int[] len = new int[numlist.length];
-        for(int i=0; i<numlist.length; i++){
-            len[i] = Math.abs(numlist[i] - n);
+        List<Pair> pairs = new ArrayList<>();
+        for (int num : numlist) {
+            pairs.add(new Pair(num, Math.abs(num - n)));
         }
-        List<Integer> list = new ArrayList<>();
-        boolean[] check = new boolean[numlist.length];
-        for(int i=0; i<numlist.length; i++){
-            int idx = -1;
-            int min = Integer.MAX_VALUE;
-            for(int j=0; j<numlist.length; j++){
-                if(check[j]==false && len[j]<min){
-                    idx = j;
-                    min = len[idx];
-                }
-                else if(check[j] == false && len[j] == min){
-                    idx = numlist[idx]>numlist[j] ? idx : j;
-                }
-            }
-            check[idx] = true;
-            list.add(numlist[idx]);
+
+        Collections.sort(pairs); // 정의된 compareTo에 따라 정렬
+
+        List<Integer> result = new ArrayList<>();
+        for (Pair p : pairs) {
+            result.add(p.value);
         }
-        return list;
+        return result;
     }
 }
