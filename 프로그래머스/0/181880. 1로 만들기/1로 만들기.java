601dead2b4324e645a9d1d0f1900790e3f507ae5
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-06-28 09:20:35 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,30 +1,12 @@
 class Solution {
     public int solution(int[] num_list) {
-        int[] cnt = new int[num_list.length];
-        while(true){
-        boolean check = true;
-        for(int i=0; i<num_list.length; i++){
-            if(num_list[i]%2 ==0){
-                num_list[i] /= 2;
-                cnt[i]++;
-            }
-            else if(num_list[i] %2 !=0 && num_list[i] != 1){
-                num_list[i] = (num_list[i] - 1) / 2;
-                cnt[i]++;
+        int answer = 0;
+        for(int i = 0;num_list.length>i;i++){
+            while(num_list[i]!=1){
+                num_list[i]/=2;
+                answer++;
             }
         }
-        for(int i : num_list){
-            if(i != 1){
-                check = false;
-                break;
-            }
-        }
-            if(check) break;
-        }
-        int ans = 0;
-        for(int c: cnt){
-            ans += c;
-        }
-        return ans;
+        return answer;
     }
 }
