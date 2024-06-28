6121539b04e92e09ccc9aebc9247b9c8fb010b90
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-06-28 14:28:44 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,17 +1,16 @@
 class Solution {
     public int solution(int[] common) {
-        int d1 =0, d2=0, r1=0, r2=0;
-        d1 = common[1] - common[0];
-        d2 = common[2] - common[1];
-        if(common[0]!=0 && common[1] != 0){
-            r1 = common[1]/common[0];
-            r2 = common[2]/common[1];
+        int answer = 0;
+
+        int x = common[1] - common[0];
+        int y = common[2] - common[1];
+
+        if (x == y) {
+            answer = common[common.length - 1] + y;
+        } else {
+            answer = common[common.length - 1] * common[2] / common[1];
         }
-        if(d1 == d2){
-            return common[common.length-1] + d1;
-        }else if(r1 == r2){
-            return common[common.length-1] * r1;
-        }
-        return 0;
+
+        return answer;
     }
 }
